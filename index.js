f4a3a0451fa4dc189269bd4599793ed4ef9ccc5f
michaeljwilliams <michael@williams.blue>
2017-12-27 03:22:46 -0800

    Google limits to 60 results max

--- index.js
+++ index.js
@@ -6,16 +6,45 @@ var data
 
 
 
-function placeNearbySearch(location, numResults) {
+// Search for Places in a specified radius from the given location.
+// searchRadius is in meters. There are about 1600 meters in a mile
+function placeNearbySearch(lat, long, searchRadius) {
+    var data = {}
+    var i = 1
 
+    httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${searchRadius}&key=${settings.googleMapsApiKey}`,
+        (dataChunk) => {
+            collectData(dataChunk)
+            if(dataChunk.next_page_token) continueSearch(dataChunk.next_page_token)
+        })
+
+    function collectData(dataChunk) {
+        // EXTRACT DESIRED DATA, THEN ADD TO data
+        // data += dataChunk
+        console.log(dataChunk)
+        console.log(`\n\n\nPage ${i}\n\n\n`); i++
+    }
+
+    async function continueSearch(pagetoken) {
+        await wait(1500)
+        httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${settings.googleMapsApiKey}&pagetoken=${pagetoken}`,
+            (dataChunk) => {
+                collectData(dataChunk)
+                if(dataChunk.next_page_token) continueSearch(dataChunk.next_page_token)
+            })
+    }
 }
 
+// Gets place details, given a Place ID
 function getPlaceDetails(placeid) {
-    var parsedData = httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${settings.googleMapsApiKey}`, (parsedData) => {
-        console.log(parsedData)
-    })
+    httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${settings.googleMapsApiKey}`,
+        (data) => {
+            console.log(data)
+        })
 }
 
+
+// Get request with given URL. Should return JSON, which is parsed and passed to the callback.
 function httpsGetJson(url, cb) {
     https.get(url, (response) => {
         const { statusCode } = response
@@ -50,6 +79,13 @@ function httpsGetJson(url, cb) {
     })
 }
 
+// Wait for x ms. Use with await in async funcs
+function wait(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
 
 
 console.log(`
@@ -58,7 +94,9 @@ console.log(`
 |          by Michael Williams         |
 +--------------------------------------+
 `)
-getPlaceDetails("ChIJEwLrfRTo3IARBvSE67C43KQ")
+
+placeNearbySearch(33.647862, -117.715524, 1000)
+//getPlaceDetails("ChIJEwLrfRTo3IARBvSE67C43KQ")
 
 
 
