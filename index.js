3b1f321c7c9d7e6e02d86a46d66ad4295b54245f
michaeljwilliams <michael@williams.blue>
2017-12-31 22:36:08 -0800

    adding 60 place details to DATA

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
 var https = require('https')
+const util = require('util') // For inspecting objects
 
-//
-const settings = require('./settings.json')
-var data
+// Add your Google Maps API Key to this file
+const apikey = require('./google_maps_api_key.json.json')
+var DATA = {}
 
 
 
 // Search for Places in a specified radius from the given location.
 // searchRadius is in meters. There are about 1600 meters in a mile
 function placeNearbySearch(lat, long, searchRadius) {
-    var data = {}
-    var i = 1
+    var placeNumber = 1
 
-    httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${searchRadius}&key=${settings.googleMapsApiKey}`,
+    httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${searchRadius}&key=${apikey.key}`,
         (dataChunk) => {
             collectData(dataChunk)
-            if(dataChunk.next_page_token) continueSearch(dataChunk.next_page_token)
         })
 
-    function collectData(dataChunk) {
-        // EXTRACT DESIRED DATA, THEN ADD TO data
-        // data += dataChunk
-        console.log(dataChunk)
-        console.log(`\n\n\nPage ${i}\n\n\n`); i++
+    // Collects data and adds to DATA
+    function collectData(data) {
+        for(let eachPlace of data.results) {
+            let placeID = eachPlace.place_id
+            // If this place doesn't already exist in DATA
+            if(!DATA.placeID) {
+                // Gets place details, given a Place ID, and adds to DATA
+                httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${apikey.key}`,
+                (place) => {
+                    // Don't collect data if place no longer exists
+                    if(!place.permanently_closed) {
+                        let p = place.result
+                        DATA.placeID = {
+                            "placeID": placeID,
+                            "name": p.name,
+                            "address": p.formatted_address,
+                            "phone": p.formatted_phone_number,
+                            "website": p.website,
+                            "internationalPhone": p.international_phone_number,
+                            "latitude": p.geometry.location.lat,
+                            "longitude": p.geometry.location.lng,
+                            "googlePage": p.url,
+                            "hours": p.opening_hours,
+                            "priceLevel": p.price_level,
+                            "rating": p.rating,
+                            "types": p.types,
+                            "utcOffset": p.utc_offset,
+                            "vicinity": p.vicinity
+                        }
+                        console.log(`${placeNumber}. ${DATA.placeID.name}`)
+                        placeNumber++
+                    }
+                })
+            }
+        }
+        // await?   if(data.next_page_token) continueSearch(data.next_page_token)
     }
 
+    // Continues search if more than 20 results
     async function continueSearch(pagetoken) {
         await wait(1500)
-        httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${settings.googleMapsApiKey}&pagetoken=${pagetoken}`,
+        httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apikey.key}&pagetoken=${pagetoken}`,
             (dataChunk) => {
+                console.log(`dataChunk: ${dataChunk}`)
                 collectData(dataChunk)
-                if(dataChunk.next_page_token) continueSearch(dataChunk.next_page_token)
             })
     }
 }
-
-// Gets place details, given a Place ID
-function getPlaceDetails(placeid) {
-    httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=${settings.googleMapsApiKey}`,
-        (data) => {
-            console.log(data)
-        })
-}
-
 
 // Get request with given URL. Should return JSON, which is parsed and passed to the callback.
 function httpsGetJson(url, cb) {
@@ -96,7 +118,7 @@ console.log(`
 `)
 
 placeNearbySearch(33.647862, -117.715524, 1000)
-//getPlaceDetails("ChIJEwLrfRTo3IARBvSE67C43KQ")
+// console.log(`\n\n\nDATA: \n\n${util.inspect(DATA, false, null)}`)
 
 
 
