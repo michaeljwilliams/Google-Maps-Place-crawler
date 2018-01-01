807b7659b04022258bb5991c760e274a8a3590ab
michaeljwilliams <michael@williams.blue>
2018-01-01 02:22:58 -0800

    asynchronicity

--- index.js
+++ index.js
@@ -2,73 +2,76 @@ var https = require('https')
 const util = require('util') // For inspecting objects
 
 // Add your Google Maps API Key to this file
-const apikey = require('./google_maps_api_key.json.json')
+const apikey = require('./google_maps_api_key.json')
+
 var DATA = {}
-
-
 
 // Search for Places in a specified radius from the given location.
 // searchRadius is in meters. There are about 1600 meters in a mile
-function placeNearbySearch(lat, long, searchRadius) {
-    var placeNumber = 1
+async function placeNearbySearch(lat, long, searchRadius) {
+    let placeNumber = 1
 
-    httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${searchRadius}&key=${apikey.key}`,
+    await httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${searchRadius}&key=${apikey.key}`,
         (dataChunk) => {
             collectData(dataChunk)
         })
 
     // Collects data and adds to DATA
-    function collectData(data) {
-        for(let eachPlace of data.results) {
+    async function collectData(dataChunk) {
+        for(let eachPlace of dataChunk.results) {
             let placeID = eachPlace.place_id
+            console.log(`${placeNumber}. ${placeID}`)
+            placeNumber++
+
             // If this place doesn't already exist in DATA
             if(!DATA.placeID) {
                 // Gets place details, given a Place ID, and adds to DATA
-                httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${apikey.key}`,
-                (place) => {
-                    // Don't collect data if place no longer exists
-                    if(!place.permanently_closed) {
-                        let p = place.result
-                        DATA.placeID = {
-                            "placeID": placeID,
-                            "name": p.name,
-                            "address": p.formatted_address,
-                            "phone": p.formatted_phone_number,
-                            "website": p.website,
-                            "internationalPhone": p.international_phone_number,
-                            "latitude": p.geometry.location.lat,
-                            "longitude": p.geometry.location.lng,
-                            "googlePage": p.url,
-                            "hours": p.opening_hours,
-                            "priceLevel": p.price_level,
-                            "rating": p.rating,
-                            "types": p.types,
-                            "utcOffset": p.utc_offset,
-                            "vicinity": p.vicinity
+
+                await httpsGetJson(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${apikey.key}`,
+                    (place) => {
+                        // Don't collect data if place no longer exists
+                        if(!place.permanently_closed) {
+                            let p = place.result
+                            DATA.placeID = {
+                                "placeID": placeID,
+                                "name": p.name,
+                                "address": p.formatted_address,
+                                "phone": p.formatted_phone_number,
+                                "website": p.website,
+                                "internationalPhone": p.international_phone_number,
+                                "latitude": p.geometry.location.lat,
+                                "longitude": p.geometry.location.lng,
+                                "googlePage": p.url,
+                                "hours": p.opening_hours,
+                                "priceLevel": p.price_level,
+                                "rating": p.rating,
+                                "types": p.types,
+                                "utcOffset": p.utc_offset,
+                                "vicinity": p.vicinity
+                            }
                         }
-                        console.log(`${placeNumber}. ${DATA.placeID.name}`)
-                        placeNumber++
+                        console.log(`${placeNumber}. ${DATA.name}`)
                     }
-                })
+                )
             }
         }
-        // await?   if(data.next_page_token) continueSearch(data.next_page_token)
+        if(dataChunk.next_page_token) return continueSearch(dataChunk.next_page_token)
     }
 
     // Continues search if more than 20 results
     async function continueSearch(pagetoken) {
-        await wait(1500)
-        httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apikey.key}&pagetoken=${pagetoken}`,
+        wait(1500)
+        await httpsGetJson(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apikey.key}&pagetoken=${pagetoken}`,
             (dataChunk) => {
-                console.log(`dataChunk: ${dataChunk}`)
                 collectData(dataChunk)
             })
     }
+    return DATA
 }
 
 // Get request with given URL. Should return JSON, which is parsed and passed to the callback.
-function httpsGetJson(url, cb) {
-    https.get(url, (response) => {
+async function httpsGetJson(url, cb) {
+    await https.get(url, (response) => {
         const { statusCode } = response
         const contentType = response.headers['content-type']
 
@@ -117,10 +120,12 @@ console.log(`
 +--------------------------------------+
 `)
 
-placeNearbySearch(33.647862, -117.715524, 1000)
+placeNearbySearch(33.647862, -117.715524, 20)
 // console.log(`\n\n\nDATA: \n\n${util.inspect(DATA, false, null)}`)
 
-
+wait(5000).then( () => {
+    console.log(`\n\n\nDATA: \n\n${util.inspect(DATA, false, null)}`)
+})
 
 module.exports = {
 
