f87e03d7f3c41d353e3b630e39caceea33d6af32
michaeljwilliams <michael@williams.blue>
2018-01-02 21:51:26 -0800

    add test with examples

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
 const util = require('util') // For inspecting objects
 
-const placeNearbySearch = (require('./index')).placeNearbySearch
+// Not const because it posesses the data container, which is manipulated
+let GoogleMapsPlaceCrawler = (require('./index')).GoogleMapsPlaceCrawler
+
 // Add your Google Maps API Key to this file
 const apikey = (require('./google_maps_api_key.json')).key
+
+
+
+// Test function that does something with the data (displays Place names)
+function tester() {
+    // List the names of all the places
+    console.log(`\nFound these places:\n`)
+    for(let eachPlace in GoogleMapsPlaceCrawler.data) {
+        console.log(util.inspect(GoogleMapsPlaceCrawler.data[eachPlace].name, false, null))
+    }
+
+    console.log(`\n${util.inspect(GoogleMapsPlaceCrawler.data, false, null)}`)
+
+    let numPlaces = Object.keys(GoogleMapsPlaceCrawler.data).length
+    console.log(`\nFound ${numPlaces} places\n`)
+
+    return GoogleMapsPlaceCrawler.data
+}
+
+
 
 console.log(`
 +--------------------------------------+
@@ -13,15 +35,10 @@ console.log(`
 Using Google Maps API key: ${apikey}
 `)
 
-placeNearbySearch(33.647862, -117.715524, 20).then( (DATA) => {
-    /* List the names of all the places. Doesn't work for some reason (ReferenceError: name is not defined)
-    for(let eachPlace in DATA) {
-        console.log(util.inspect(DATA[eachPlace][name], false, null))
-    }
-    */
-    console.log(`\n${util.inspect(DATA, false, null)}`)
+// placeNearbySearch example
+// args: latitude, longitude, search radius (meters)
+// GoogleMapsPlaceCrawler.placeNearbySearch(33.647862, -117.715524, 20).then(tester())
 
-    let numPlaces = Object.keys(DATA).length
-    console.log(`\nFound ${numPlaces} places\n`)
-    if(numPlaces === 60) console.log("Google has a limit of 60 results for each specific search. Change lat/lng to obtain more results.\n")
-})
+// searchArea example
+// args: start latitude, start longitude, end latitude, end longitude, search radius (meters)
+GoogleMapsPlaceCrawler.searchArea(33.638787, -117.724922, 33.641797, -117.721081, 100).then(tester())
